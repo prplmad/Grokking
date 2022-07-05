800bd12935a6440a8f4eb6848a29fbe5e8e21b43
prplmad <spacedo6pblu@gmail.com>
2022-07-05 13:16:20 +0300

    SelectionSort

--- GrokkingAlgorithms/Program.cs
+++ GrokkingAlgorithms/Program.cs
@@ -1,12 +1,23 @@
 ﻿using GrokkingAlgorithms;
 using System;
 
-//Binary Search
-//O(Log n)
-int[] sortedArray = new int[] { 1, 5, 8, 10, 15, 17, 100, 102, 105, 203, 2005, 5001, 10000 };
-Console.WriteLine(BinarySearch.FindIndex(sortedArray, 100)); // 6 
-Console.WriteLine(BinarySearch.FindIndex(sortedArray, 200)); // null
-Console.WriteLine(BinarySearch.FindIndex(sortedArray, 10000)); // 12
-Console.WriteLine(BinarySearch.FindIndex(sortedArray, 1)); // 0
-Console.WriteLine(BinarySearch.FindIndex(sortedArray, 2005)); // 10
+////Binary Search
+////O(Log n)
+//int[] sortedArray = new int[] { 1, 5, 8, 10, 15, 17, 100, 102, 105, 203, 2005, 5001, 10000 };
+//Console.WriteLine(BinarySearch.FindIndex(sortedArray, 100)); // 6 
+//Console.WriteLine(BinarySearch.FindIndex(sortedArray, 200)); // null
+//Console.WriteLine(BinarySearch.FindIndex(sortedArray, 10000)); // 12
+//Console.WriteLine(BinarySearch.FindIndex(sortedArray, 1)); // 0
+//Console.WriteLine(BinarySearch.FindIndex(sortedArray, 2005)); // 10
+
+//Selection Sort
+//O(n^2)
+
+int[] nonSortedArray = new int[] {6,2,88,1,4,7,9,106,1000,101,0,20,67,31,1001 };
+int[] newArray = SelectionSort.SelectionSorting(nonSortedArray);
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.WriteLine(newArray[i]);
+}
+
 
---
+++ GrokkingAlgorithms/SelectionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrokkingAlgorithms
+{
+    public static class SelectionSort
+    {
+
+        public static int[] SelectionSorting(int [] array)
+        {
+            for (int i = 0; i < array.Length - 2; i++)
+            {
+                int smallestIndex = i;
+                int temp = 0;
+
+                for (int j = i + 1; j < array.Length - 1; j++)
+                {
+                    if (array[smallestIndex] > array[j])
+                    {
+                        smallestIndex = j;
+                    }
+                }
+                temp = array[i];
+                array[i] = array[smallestIndex];
+                array[smallestIndex] = temp;
+            }
+            return array;
+        }
+    }
+}
