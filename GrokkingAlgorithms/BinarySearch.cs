72a688b4bbc3efefb2440cb3f99347600d5d5363
prplmad <spacedo6pblu@gmail.com>
2022-07-05 10:24:58 +0300

    init

---
+++ GrokkingAlgorithms/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrokkingAlgorithms
+{
+    public static class BinarySearch
+    {
+        //Log2N
+        public static object FindIndex(int[] array, int number)
+        {
+            int low = 0;
+            int high = array.Length - 1;
+            int mid;
+            int guess;
+
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                guess = array[mid];
+                if (guess == number)
+                    return mid;
+                if (guess > number)
+                    high = mid - 1;
+                if (guess < number)
+                    low = mid + 1;
+            }
+            return "Значение не найдено";
+        }
+
+    }
+}
---
+++ GrokkingAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using GrokkingAlgorithms;
+using System;
+
+//Binary Search
+//O(Log n)
+int[] sortedArray = new int[] { 1, 5, 8, 10, 15, 17, 100, 102, 105, 203, 2005, 5001, 10000 };
+Console.WriteLine(BinarySearch.FindIndex(sortedArray, 100)); // 6 
+Console.WriteLine(BinarySearch.FindIndex(sortedArray, 200)); // null
+Console.WriteLine(BinarySearch.FindIndex(sortedArray, 10000)); // 12
+Console.WriteLine(BinarySearch.FindIndex(sortedArray, 1)); // 0
+Console.WriteLine(BinarySearch.FindIndex(sortedArray, 2005)); // 10
+
